415a820d5ce48441967ca31ea6752cff35449cae
brysah <brysahvidal@gmail.com>
2024-05-10 16:22:03 -0300

    feat: add benefits section

--- landingpage/src/App.jsx
+++ landingpage/src/App.jsx
@@ -1,5 +1,9 @@
 import logo from './assets/logo.png'
 import dashboard from './assets/dashboard.png'
+import { Benefits } from './components/Benefits'
+import benefit1 from './assets/benefit-1.png'
+import benefit2 from './assets/benefit-2.png'
+import benefit3 from './assets/benefit-3.png'
 
 function App() {
   return (
@@ -31,9 +35,38 @@ function App() {
             <button className='bg-primary-500 px-[31px] py-[14px] rounded-[30px] text-primary font-semibold text-[16px] hover:bg-primary-400' >Get a Free Demo</button>
             <button className='bg-secondary-500 py-[14px] px-[24px] rounded-[30px] text-secondary-200 font-semibold text-[16px] hover:bg-secondary-400' >See Princing</button>
           </div>
-          <img src={dashboard} alt="dashboard" className='w-full object-cover'/>
+          <img src={dashboard} alt="dashboard" className='w-full object-cover' />
         </div>
       </main>
+      <section className='bg-primary pt-[120px] max-w-screen-xl w-full mx-auto px-[15px] mb-[120px]' >
+        <div className="flex max-w-[904px] items-center justify-between mb-[79px]">
+          <div>
+            <h4 className='text-primary-500 font-semibold text-[20px] mb-2'>WHY USE SPEND.IN</h4>
+            <h3 className='text-secondary-900 font-bold text-[40px]'>Easy, Simple, Affordable</h3>
+          </div>
+          <div>
+          <p className='max-w-[496px] text-secondary-400 text-[20px]'>Our platform helps your business in managing expenses. These are some of the reasons why you should use our platform in managing business finances.</p>
+          </div>
+        </div>
+        
+        <div className="grid w-full grid-cols-3 gap-[24px]">
+            <Benefits 
+            name='Automatic Invoice Payment'
+            description="No need to pay manually, we provide automatic invoice payment service! Set a payment schedule and you're done, it's that easy!"
+            imgSrc={benefit1}
+            />
+            <Benefits 
+            name='Clear payment history'
+            description="Still writing manual expenses? Our platform breaks down every expense you log down to the millisecond!"
+            imgSrc={benefit2}
+            />
+            <Benefits 
+            name='Use of multi-card payments'
+            description="Have more than 1 bank account or credit/debit card? Our platform is already integrated with many banks around the world, for easier payments!"
+            imgSrc={benefit3}
+            />
+          </div>
+      </section>
     </>
   )
 }
---
+++ landingpage/src/components/Benefits/index.jsx
@@ -0,0 +1,12 @@
+export function Benefits({imgSrc,name,description}){
+    return(
+        <>
+            <div>
+                <img src={imgSrc} alt='benefit card' className="w-full object-cover mb-[24px]"/>
+                 <h5 className="text-secondary-900 font-semibold text-[24px] mb-[12px]" >{name}</h5>
+                 <p className="text-secondary-400 text-[16px]" >{description}</p>
+            </div>
+        </>
+    )
+}
+
