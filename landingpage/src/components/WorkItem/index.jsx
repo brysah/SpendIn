92b68f4712ae8305d32c58526a645cd28431b43f
brysah <brysahvidal@gmail.com>
2024-05-10 17:21:07 -0300

    fix: workitem circle

--- landingpage/src/components/WorkItem/index.jsx
+++ landingpage/src/components/WorkItem/index.jsx
@@ -3,7 +3,7 @@ export function WorkItem({ imgSrc,text,index }) {
         <>
             <div className="w-full max-w-[200px] mx-auto">
                 <div className="relative mb-[24px]">
-                    <div className="w-[50px] rounded-full h-[50px] bg-primary-500 font-semibold text-primary text-center text-[24px] absolute top-[-20px] left-[20px] border-4 border-secondary-700">{index}</div>
+                    <div className="w-[50px] rounded-full h-[50px] bg-primary-500 font-semibold text-primary text-center text-[24px] absolute top-[-20px] left-[-20px] border-4 border-secondary-700">{index}</div>
                     <img src={imgSrc} alt="how it works" className="object-cover mx-auto"/>
                 </div>
                 <p className="font-medium text-[20px] text-primary text-center">{text}</p>
