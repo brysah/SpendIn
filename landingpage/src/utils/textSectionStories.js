883318cfc8524b320b50cfaa6c68a8969106c093
brysah <brysahvidal@gmail.com>
2024-05-10 18:33:32 -0300

    feat: add utils text for section Stories

---
+++ landingpage/src/utils/textSectionStories.js
@@ -0,0 +1,22 @@
+import goodStatistics from '../assets/spend-Statistics-1.png'
+import badStatistics from '../assets/spend-Statistics-2.png'
+
+export const withSpend = {
+    title:'Track Business Expenses until its Milisecond',
+    pros:[
+        'Analyze your business cost easily with group transaction thorugh tagging feature.',
+        'Add more than one card for payment. Integrated with more than 50+ payment method and support bulk payment.',
+        'Arrange your business expenses by date, name, etc.,  with just one click.'
+    ],
+    imageStatistics:{goodStatistics}
+}
+
+export const withoutSpend = {
+    title:'Taking too long to tidy up administrative files makes it unproductive',
+    pros:[
+        'Complex recording process due to every administrative file in a different place.',
+        'Need more effort to pay manually one by one invoice because there is no payment accommodation.',
+        'Manual data arranging needs a long time because the different months/years are not in the same place.'
+    ],
+    imageStatistics:{badStatistics}
+}
